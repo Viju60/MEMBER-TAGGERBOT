2142706c425034fb461a3daa2a45c4c46c5b52bf
I AM VIJAY ࿐ <87687583+Viju60@users.noreply.github.com>
2021-10-27 11:04:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,26 +21,26 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-    "__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+    "**I AM MEMBER TAGGER BOT**, **I CAN MENTION ALMOST ALL MEMBERS IN GROUP OR CHANNEL** 👻\nClick **/help** **for more information** \n\n 🤖 **MY DEVELOPER IS** [𓄂I AM᭄ VIJAY ࿐](https://t.me/VIJAY1142)",
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('✅ ᴜᴘᴅᴀᴛᴇ ᴀɴᴅ ɴᴇᴡꜱ', 'https://t.me/VkTgBotz'),
+        Button.url('ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤝', 'https://t.me/VkTgBotSupport')
       ]
     )
   )
 
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
+  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\n REPO [MEMBER TAGGER BOT](https://github.com/Viju60/MentionAllBot)"
   await event.reply(
     helptext,
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('✅ ᴜᴘᴅᴀᴛᴇ ᴀɴᴅ ɴᴇᴡꜱ', 'https://t.me/VkTgBotz'),
+        Button.url('ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤝', 'https://t.me/VkTgBotSupport')
       ]
     )
   )
